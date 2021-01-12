dc2fef10aaaea961bb13204ef0a9cc446b79fb06
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-12 14:07:12 -0300

    Progress Bar carregando conforme o arquivo sobe para nuvem

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,6 +3,7 @@ class DropBoxController{
         this.btnSendFileEl = document.querySelector("#btn-send-file");
         this.inputFilesEl = document.querySelector("#files");
         this.snackModalEl = document.querySelector("#react-snackbar-root");
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
 
         this.initEvents();
     }
@@ -37,8 +38,8 @@ class DropBoxController{
                     reject(event)
                 }
 
-                ajax.onprogress = event => {
-                    console.log(event)
+                ajax.upload.onprogress = event => {
+                    this.uploadProgress(event, file);
                 }
 
                 let formData = new FormData();
@@ -48,5 +49,13 @@ class DropBoxController{
         })
 
         return Promise.all(promises)
+    }
+
+    uploadProgress(event, file){
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt((loaded / total) * 100)
+
+        this.progressBarEl.style.width = `${porcent}%`
     }
 }
