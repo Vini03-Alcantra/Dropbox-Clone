0286e41dd621d2157b82d0c649ac44065c13d4aa
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-12 14:32:58 -0300

    Criando método formatTimeToHuman para exibir tempo que falta para o arquivo subir

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -4,6 +4,8 @@ class DropBoxController{
         this.inputFilesEl = document.querySelector("#files");
         this.snackModalEl = document.querySelector("#react-snackbar-root");
         this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
+        this.nameFileEl = this.snackModalEl.querySelector('.filename');
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft');
 
         this.initEvents();
     }
@@ -44,6 +46,7 @@ class DropBoxController{
 
                 let formData = new FormData();
                 formData.append('input-file', file);
+                this.startUploadTime = Date.now()
                 ajax.send(formData);
             }))
         })
@@ -52,10 +55,32 @@ class DropBoxController{
     }
 
     uploadProgress(event, file){
+        let timespent = Date.now()
         let loaded = event.loaded;
         let total = event.total;
-        let porcent = parseInt((loaded / total) * 100)
-
+        let porcent = parseInt((loaded / total) * 100)        
+        let timeleft = ((100 - porcent) * timespent) / porcent
         this.progressBarEl.style.width = `${porcent}%`
+        this.nameFileEl.innerHTML = file.name;
+        this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeleft);
+        
+    }
+
+    formatTimeToHuman(duration){
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24)
+
+        if (hours > 0) {
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+        }
+        if (minutes > 0) {
+            return `${minutes} minutos e ${segundos}`;
+        }
+        if (seconds > 0) {
+            return `${seconds} segundos`;
+        }
+
+        return "";
     }
 }
